ca0918a0914942eacfbdc50966b9e5513781fbd6
Virendra <virendra.ims@gmail.com>
2020-06-03 20:08:39 +0530

    Added changes for disabling otp and updating user epass status

--- src/main/java/com/med/disease/tracking/app/util/otp/OTPUtil.java
+++ src/main/java/com/med/disease/tracking/app/util/otp/OTPUtil.java
@@ -20,16 +20,17 @@ public class OTPUtil {
 	public static String sendOtp(String mobileNumber) {
 
 		// Generate random integers in range 0 to 9999
-		String otpCode = OTPGenerator.generate();
-		String message = "4 digit OTP for COVID-APP is " + otpCode;
+//		String otpCode = OTPGenerator.generate();
+		String otpCode = "1234";
+//		String message = "4 digit OTP for COVID-APP is " + otpCode;
 //		String mobileNumber = "10-digit-mobile-number";
-
-		OTPResponse otpResponse = OTPSender.send(message, mobileNumber);
-
-		if (!otpResponse.isSuccess()) {
-			// Save OTP value to DB against User record
-			otpCode = "0";
-		} 
+//uncomment below code to enable sending otp
+//		OTPResponse otpResponse = OTPSender.send(message, mobileNumber);
+//
+//		if (!otpResponse.isSuccess()) {
+//			// Save OTP value to DB against User record
+//			otpCode = "0";
+//		} 
 		return otpCode;
 	}
 }
